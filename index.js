e5ff0486603bb0a50caabe991f42a63a0cea66e5
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-07-03 10:08:57 -0400

    Upload of React Practice
    Practice documents based off of the first 10 videos of Angela Yu's React in 2022 Bootcamp. 391-400.

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>My Man!</h1>, document.getElementById('root'));
+
+// might need to keep up with localhost:3000 on browser. VSCode doesn't seem to open into it.
