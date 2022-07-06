d056a957b971396f8c1e7e0743d920251b30291d
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-07-06 15:06:00 -0400

    Add files via upload
    This activity was definitely challenging once it got to step four. I was trying to do too much and make a for function to try and go through the list of names, however a simpler approach is what was required. In the end I learned how to call objects within an array to be used within the props element.

---
+++ Props Quiz/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//1. Apply CSS styles to App.jsx component... CHECK!!!
+//to match the appearance on the completed app: CHECK!!!
+//https://c6fkx.csb.app/ CHECK!!!!
+//2. Extract the contact card as a re`usable Card component. CHECK!!!
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts. CHECK!!!
+//4. Import the contacts.js file to create card components.
