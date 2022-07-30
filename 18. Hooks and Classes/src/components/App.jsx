6dff6fda1d42c36c0a373aba6b9f6c780d66dd88
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-07-30 07:40:28 -0400

    Included notes regarding classes vs hooks.

--- 18. Hooks and Classes/src/components/App.jsx
+++ 18. Hooks and Classes/src/components/App.jsx
@@ -9,3 +9,11 @@ class App extends React.Component {
 }
 
 export default App;
+
+//classes are not called like a function. Can remove parenthesis. Must extend into a React Component. Component is rendered in a render method. Using classes was originally done in order to use states in React.
+// https://reactjs.org/docs/state-and-lifecycle.html 
+
+// Creating hooks in 2018 was to limit the amount bindings and additional work that had to take place in order to use the useState.bind
+// https://reactjs.org/docs/hooks-intro.html
+
+// Hooks are encouraged by the react team for new code. It essentially hides some of the boilerplate/setup that would be required in creating a useState. [1, 2] the 1st value that we use currently originally had to be set up and created with values being bound to it. Not to mention that it also uses 'this'.
