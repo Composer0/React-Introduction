4f199f975b537280609fe7f3a95c6a13593ddd1f
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-07-31 23:26:47 -0400

    Updating Form with Objects
    Learned how to create an object while using states. Form now updates the "prevValue" to the newValue which is equal to event.target.value. The value unaffected is taken not of. Example. If the input occurs on the first name, last name will be set to prevValue.lName. It references that portion of the object whether it is blank or it has been modified with a new newValue. inputName logs which field is being used as its event targets the "name" of the input. fName and lName. That is why we are setting the conditional statement to read the input name first in order to decide what needs to be changed and what needs to be left alone.

--- 19. Changing Complex State/src/components/App.jsx
+++ 19. Changing Complex State/src/components/App.jsx
@@ -2,23 +2,42 @@ import React, {useState} from "react";
 
 function App() {
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [headingTextFirst, setHeadingTextFirst] = useState("");
-  const [headingTextLast, setHeadingTextLast] = useState("");
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+ 
   const [isMousedOver, setMouseOver] = useState(false);
 
-  function firstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
 
-  function lastNameChange(event) {
-    setLastName(event.target.value);
+    // if (inputName === "fName") {
+    //   setFullName({fName: newValue}, );
+    // } else if (inputName === "lName") {
+    //   setFullName({lName: newValue})
+    // }  Doesn't work. It will reset the opposite portion of the object each time an input is made.
+
+    setFullName(prevValue => {
+      console.log(prevValue);
+      if (inputName === "fName") {
+        return {
+          fName: newValue,
+          lName: prevValue.lName
+        }
+      } else if (inputName === "lName") {
+          return {
+            fName: prevValue.fName,
+            lName: newValue
+          }
+        }
+      }
+    )
   }
 
   function handleClick(event) {
-    setHeadingTextFirst(firstName);
-    setHeadingTextLast(lastName);
     event.preventDefault();
   }
 
@@ -34,20 +53,21 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Hello {headingTextFirst} {headingTextLast}</h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <form onSubmit={handleClick}>
+
         <input 
-        name="fName" 
-        placeholder="First Name" 
-        onChange={firstNameChange}
-        value={firstName}
+          name="fName" 
+          placeholder="First Name" 
+          onChange={handleChange}
+          value={fullName.fName}
         />
 
         <input 
-        name="lName" 
-        placeholder="Last Name" 
-        onChange={lastNameChange}
-        value={lastName}
+          name="lName" 
+          placeholder="Last Name" 
+          onChange={handleChange}
+          value={fullName.lName}
         />
 
         <button 
@@ -56,7 +76,7 @@ function App() {
             onMouseOver={handleMouseOver}
             onMouseOut={handleMouseOut}>
             Submit
-            </button>
+        </button>
       
       </form>
     </div>
